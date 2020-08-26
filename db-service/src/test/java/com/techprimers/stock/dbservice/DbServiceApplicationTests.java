053f76e36b1576d26059365f9ef6abe391f7ef1d
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 16:46:10 +0300

    disable test files for dockerizing

--- db-service/src/test/java/com/techprimers/stock/dbservice/DbServiceApplicationTests.java
+++ db-service/src/test/java/com/techprimers/stock/dbservice/DbServiceApplicationTests.java
@@ -1,11 +1,11 @@
 package com.techprimers.stock.dbservice;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DbServiceApplicationTests {
-
-}
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DbServiceApplicationTests {
+//
+//}
