fbc4935d71a90031e80abd71bbde7ebb1e050de4
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 14:57:33 +0300

    Quote deleteById api impl

--- db-service/src/main/java/com/techprimers/stock/dbservice/repository/QuotesRepository.java
+++ db-service/src/main/java/com/techprimers/stock/dbservice/repository/QuotesRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+
+    Optional<Quote> findById(Integer id);
 
     List<Quote> findByUserName(String username);
 
--- db-service/src/main/java/com/techprimers/stock/dbservice/resource/DbServiceResource.java
+++ db-service/src/main/java/com/techprimers/stock/dbservice/resource/DbServiceResource.java
@@ -1,5 +1,6 @@
 package com.techprimers.stock.dbservice.resource;
 
+import com.techprimers.stock.dbservice.exception.QuoteNotFoundException;
 import com.techprimers.stock.dbservice.model.Quote;
 import com.techprimers.stock.dbservice.model.Quotes;
 import com.techprimers.stock.dbservice.repository.QuotesRepository;
@@ -33,16 +34,24 @@ public class DbServiceResource {
     }
 
     @PostMapping("/delete/{username}")
-    public List<String> delete(@PathVariable("username") final String username) {
+    public List<String> deleteByUsername(@PathVariable("username") final String username) {
         List<Quote> quotes = quotesRepository.findByUserName(username);
         quotesRepository.delete(quotes);
 
         return getQuotesByUserName(username);
     }
 
+    @DeleteMapping("/delete/{id}")
+    public boolean deleteById(@PathVariable("id") final Integer id) {
+        Quote quote = quotesRepository.findById(id)
+                .orElseThrow(() -> new QuoteNotFoundException("Quote Not Found with id : " + id));
+        quotesRepository.delete(quote);
+        return true;
+    }
+
     private List<String> getQuotesByUserName(String username) {
         List<String> allUsers = quotesRepository.getAllUsers();
-
+        // return all quotes by username or empty list for no such username
         if (allUsers.contains(username))
             return quotesRepository.findByUserName(username)
                     .stream()
