0d8555427458dea6ba175ff8bb35f4a851cbadb6
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 14:45:23 +0300

    New method added to repo for checking username

--- db-service/src/main/java/com/techprimers/stock/dbservice/repository/QuotesRepository.java
+++ db-service/src/main/java/com/techprimers/stock/dbservice/repository/QuotesRepository.java
@@ -2,9 +2,14 @@ package com.techprimers.stock.dbservice.repository;
 
 import com.techprimers.stock.dbservice.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+
     List<Quote> findByUserName(String username);
+
+    @Query("select q.userName from Quote q")
+    List<String> getAllUsers();
 }
